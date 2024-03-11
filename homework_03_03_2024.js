a8b0b177434ee8004f45aadb4342b83ab22ecbae
mkrtichg <160704770+mkrtichg@users.noreply.github.com>
2024-03-11 14:01:15 +0400

    Add files via upload

--- homework_03_03_2024.js
+++ homework_03_03_2024.js
@@ -52,6 +52,9 @@ Input                                       Output
 let checkNum = 5  // 8
 const arr = [0, -1, 0, 5, 6, 6, 5, -1, 0, 5, 6]  // [8, 8, 4, 0, 8, 0, 0, 0, 4]  
 const arr1 = [];
+
+checker(array, number, i = 0, arr1);
+
 function checker(array, number, i, arr1) {
 
     let result;
@@ -88,48 +91,108 @@ Input Output
 
 */
 
-let string = 'Are var far shared?';   // 3
-let substr = 'ar';
-// let string = 'Facebook looks odd';  // 2
-// let substr = 'ook';
+// let string = 'Are var far shared?';   // 3
+// let substr = 'ar';
+let string = 'Facebook looks odd';  // 2
+let substr = 'ook';
+let result;
 
 const str = {
     string,
     substr,
-    substring: function(startIndex, endIndex){
+    substring: function (startIndex, endIndex) {
         let sub = this.string.slice(startIndex, endIndex);
         return sub
     }
 }
 
-charCount(string, substr, i = 0, count = 0)
+charCount(string, substr, i = 0, result = 0)
 
-// str.substring(startIndex, endIndex)
 
-function charCount(str, phrase, i, count) {
-    debugger
-    let result;
+
+
+function charCount(str, phrase, i) {
+    // debugger;
+
     let subs = str.substring(i, i + phrase.length)
-    if (str.length === phrase.length && str === phrase) {
-        return count = 1;
-    } else if (str.length === phrase.length && str !== phrase) {
-        return count = 0;
-    } else if (i === str.length - phrase.length) {
+    if (i === str.length - phrase.length) {
         if (subs !== phrase) {
             return
         } else {
-            return count += 1
+            return result += 1
         }
     }
 
+    charCount(str, phrase, i + 1)
+
     if (subs === phrase) {
-        return count += 1
+        result += 1
     }
 
-    charCount(str, phrase, i + 1, count)
-
-    
-
-    return result += count;
+    return result;
 
 }
+
+
+//--------------------------------------------------------------------
+
+/*
+
+4. Given a string, compute recursively (no loops) a new string where all appearances of "pi";
+have been replaced by "3.14";.
+
+Input                           Output
+"Picsart pipelines"             "3.14csart 3.14pelines"
+"picturespicturespictures"      "3.14ctures3.14ctures3.14ctures"
+
+
+*/
+
+let givenStr = "Picsart pipelines"; //            "3.14csart 3.14pelines"
+// let givenStr ="picturespicturespictures"; //     "3.14ctures3.14ctures3.14ctures"
+let newStr;
+
+funnyPi(givenStr, i = 0, newStr = '');
+
+function funnyPi(str, i) {
+    debugger;
+    // str = str.toLowerCase();
+    // let newStr = '';
+    if (i === str.length) {
+        if (str[i] + str[i + 1] !== "pi" || str[i] + str[i + 1] !== "Pi") {
+            return
+        }
+    }
+
+    if (str[i] + str[i + 1] === "pi" || str[i] + str[i + 1] === "Pi") {
+        newStr += 3.14;
+        i += 2;
+    } else {
+        newStr += str[i];
+        i += 1
+    }
+
+    funnyPi(str, i);
+
+    return newStr;
+
+}
+
+//--------------------------------------------------------------------------
+
+/*
+
+5. Given an array of nested arrays. Write a recursive function that flattens it. (Hint create
+function that concats arrays).
+
+Input                           Output
+[1, [3, 4, [1, 2]], 10]         [1, 3, 4, 1, 2, 10]
+[14, [1, [[[3, []]], 1], 0]     [14, 1, 3, 1, 0]
+
+*/
+
+const givenArr = [1, [3, 4, [1, 2]], 10]; //     [1, 3, 4, 1, 2, 10]
+// const givenArr = [14, [1, [[[3, []]], 1], 0];  //    [14, 1, 3, 1, 0]
+
+
+
